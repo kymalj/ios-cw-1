ca86a593252d266ecec37f2c44643acec9115a9c
kymalj <70738114+kymalj@users.noreply.github.com>
2020-09-09 20:53:11 -0700

    done

---
+++ CW-1-1.playground/Contents.swift
@@ -0,0 +1,20 @@
+
+var name = " khalifa aljraid "
+var hobby1 = " football "
+var hobby2 = " swimming "
+
+var age = 16
+var hight = 160
+print("My name is \(name)\n my age is: \(age)")
+
+var friendName = " Omar "
+var friendHobby1 = " football "
+var friendHobby2 = " coding "
+var friendAge = 20
+var friendHight = 167
+
+var ageDifference = age - friendAge
+var multualHobby = ( hobby1==friendHobby1 ) || (hobby1==friendHobby2) || (hobby2==friendHobby1) || (hobby2==friendHobby2)
+name == friendName
+age == friendAge
+print (multualHobby)
